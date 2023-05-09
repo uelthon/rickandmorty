92d2d7b583228ff6ed0105ce23319f3d3e53a2c1
elthon urbina <uelthon95@gmail.com>
2023-05-09 08:21:03 -0600

    image hero width and hegth

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({ image, name }) => {
   return (
     <div className='hero min-h-[90vh] bg-base-200'>
       <div className='hero-content flex-col lg:flex-row'>
-        <img src={image} className='max-w-sm rounded-lg shadow-2xl' />
+        <img width='300px' height='300px' src={image} className='max-w-sm rounded-lg shadow-2xl' />
         <div>
           <h1 className='text-5xl font-bold'>{name}</h1>
           <p className='py-6'>Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
